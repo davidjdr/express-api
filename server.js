d2d0ea218192d4aa814ee982a523380dfacda6fb
David De Freitas <david.defreitas@zweicom.com>
2017-12-19 18:23:08 -0300

    agregado swagger-ui para manejo manual de la doc

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ var express = require('express'),
     bodyParser = require('body-parser');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/eventdb');
+mongoose.connect('mongodb://localhost/eventdb', {
+  useMongoClient: true,
+  connectTimeoutMS: 1000
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -24,4 +27,8 @@ routes(app);
 //habilitado servidor en puerto 3000
 app.listen(port, function() {
 	console.log('Express API escuchando, puerto 3000');
+});
+
+app.use(function(req, res) {
+  res.status(404).send({url: req.originalUrl + ' no encontrado'})
 });
